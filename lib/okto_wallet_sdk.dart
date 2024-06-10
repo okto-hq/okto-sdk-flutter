901521c1af6a0c8e4d78cf8ffc72487d2e00afbd
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-06-10 23:19:47 +0530

    bug: fix exported models

--- lib/okto_wallet_sdk.dart
+++ lib/okto_wallet_sdk.dart
@@ -13,3 +13,4 @@ export 'src/models/client/user_portfilio_activity_model.dart';
 export 'src/models/client/user_portfolio_model.dart';
 export 'src/models/client/wallet_model.dart';
 export 'src/utils/http_client.dart';
+export 'src/models/client/export_models.dart';
--- lib/src/models/client/transfer_token_model.dart
+++ lib/src/models/client/transfer_token_model.dart
@@ -1,51 +1,51 @@
-import 'dart:convert';
+  import 'dart:convert';
 
-class TransferTokenResponse {
-  final String status;
-  final TransferTokenData data;
-  TransferTokenResponse({
-    required this.status,
-    required this.data,
-  });
+  class TransferTokenResponse {
+    final String status;
+    final TransferTokenData data;
+    TransferTokenResponse({
+      required this.status,
+      required this.data,
+    });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'status': status,
-      'data': data.toMap(),
-    };
+    Map<String, dynamic> toMap() {
+      return {
+        'status': status,
+        'data': data.toMap(),
+      };
+    }
+
+    factory TransferTokenResponse.fromMap(Map<String, dynamic> map) {
+      return TransferTokenResponse(
+        status: map['status'] ?? '',
+        data: TransferTokenData.fromMap(map['data']),
+      );
+    }
+
+    String toJson() => json.encode(toMap());
+
+    factory TransferTokenResponse.fromJson(String source) => TransferTokenResponse.fromMap(json.decode(source));
   }
 
-  factory TransferTokenResponse.fromMap(Map<String, dynamic> map) {
-    return TransferTokenResponse(
-      status: map['status'] ?? '',
-      data: TransferTokenData.fromMap(map['data']),
-    );
+  class TransferTokenData {
+    final String orderId;
+    TransferTokenData({
+      required this.orderId,
+    });
+
+    Map<String, dynamic> toMap() {
+      return {
+        'order_id': orderId,
+      };
+    }
+
+    factory TransferTokenData.fromMap(Map<String, dynamic> map) {
+      return TransferTokenData(
+        orderId: map['order_id'] ?? '',
+      );
+    }
+
+    String toJson() => json.encode(toMap());
+
+    factory TransferTokenData.fromJson(String source) => TransferTokenData.fromMap(json.decode(source));
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory TransferTokenResponse.fromJson(String source) => TransferTokenResponse.fromMap(json.decode(source));
-}
-
-class TransferTokenData {
-  final String orderId;
-  TransferTokenData({
-    required this.orderId,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'order_id': orderId,
-    };
-  }
-
-  factory TransferTokenData.fromMap(Map<String, dynamic> map) {
-    return TransferTokenData(
-      orderId: map['order_id'] ?? '',
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory TransferTokenData.fromJson(String source) => TransferTokenData.fromMap(json.decode(source));
-}
