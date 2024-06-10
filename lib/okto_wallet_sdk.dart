e7b4069e3f266dfefeed7e241c4a7279e4ece75c
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-06-10 23:43:21 +0530

    exposed orderStates

--- lib/okto_wallet_sdk.dart
+++ lib/okto_wallet_sdk.dart
@@ -14,3 +14,4 @@ export 'src/models/client/user_portfolio_model.dart';
 export 'src/models/client/wallet_model.dart';
 export 'src/utils/http_client.dart';
 export 'src/models/client/export_models.dart';
+export 'src/utils/enums.dart';
