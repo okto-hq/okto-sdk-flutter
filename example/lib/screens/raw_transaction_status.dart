73163e32cb9c2f5e6713143b8ef2bfdf936148a2
utkarsh-matchmyflight <utkarsh.shrivastava@matchmyflight.com>
2024-06-16 02:15:42 +0530

    created raw transaction status page for example app

--- example/lib/screens/raw_transaction_status.dart
+++ example/lib/screens/raw_transaction_status.dart
@@ -1,4 +1,6 @@
+import 'package:example/okto.dart';
 import 'package:flutter/material.dart';
+import 'package:okto_flutter_sdk/okto_flutter_sdk.dart';
 
 class RawTransactionStatusPage extends StatefulWidget {
   const RawTransactionStatusPage({super.key});
@@ -8,6 +10,17 @@ class RawTransactionStatusPage extends StatefulWidget {
 }
 
 class _RawTransactionStatusPageState extends State<RawTransactionStatusPage> {
+  Future<RawTransactionStatusResponse>? _rawTransactionStatus;
+
+  Future<RawTransactionStatusResponse> rawTransactionStatus() async {
+    try {
+      final rawTransaction = await okto.rawTransactionStatus(orderId: 'YOUR_ORDER_ID');
+      return rawTransaction;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +35,73 @@ class _RawTransactionStatusPageState extends State<RawTransactionStatusPage> {
                 'Raw Transaction Status',
                 style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 30),
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _rawTransactionStatus = rawTransactionStatus();
+                });
+              },
+              child: const Text('View status'),
+            ),
+            Expanded(
+              child: _rawTransactionStatus == null
+                  ? Container()
+                  : FutureBuilder<RawTransactionStatusResponse>(
+                      future: _rawTransactionStatus,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator(color: Colors.white));
+                        } else if (snapshot.hasError) {
+                          return Center(child: Text('Error: ${snapshot.error}'));
+                        } else if (snapshot.hasData) {
+                          final rawTransaction = snapshot.data!;
+                          return Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: MediaQuery.sizeOf(context).height * 0.6,
+                                  child: ListView.builder(
+                                      itemCount: rawTransaction.data.jobs.length,
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                          color: Colors.blue,
+                                          margin: const EdgeInsets.all(5),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                'Network name: ${rawTransaction.data.jobs[index].networkName}',
+                                                style: const TextStyle(color: Colors.white, fontSize: 20),
+                                              ),
+                                              Text(
+                                                'Order Id : ${rawTransaction.data.jobs[index].orderId}',
+                                                style: const TextStyle(color: Colors.white, fontSize: 20),
+                                              ),
+                                              Text(
+                                                'Status : ${rawTransaction.data.jobs[index].status}',
+                                                style: const TextStyle(color: Colors.white, fontSize: 20),
+                                              ),
+                                              Text(
+                                                'Transaction Hash : ${rawTransaction.data.jobs[index].transactionHash}',
+                                                style: const TextStyle(color: Colors.white, fontSize: 20),
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      }),
+                                )
+
+                                // Add more fields here as needed
+                              ],
+                            ),
+                          );
+                        }
+                        return Container();
+                      },
+                    ),
             ),
           ],
         ),
